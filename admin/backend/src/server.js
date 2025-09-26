5b202541f76f212177d339ac2093b714ac7803f2
Sohal Ansari <sohalansari4934@gmail.com>
2025-09-26 12:03:04 +0530

    admin update

--- admin/backend/src/server.js
+++ admin/backend/src/server.js
@@ -1,6 +1,6 @@
-const app = require("./app");
-const PORT = process.env.PORT || 5001;
+// const app = require("./app");
+// const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
